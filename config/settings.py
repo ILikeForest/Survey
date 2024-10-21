02faba9648f2be83797253886eef412cbb0009b6
ILikeForest <namu90541@gmail.com>
2024-10-21 23:41:27 +0900

    ;

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['3.37.222.118']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
